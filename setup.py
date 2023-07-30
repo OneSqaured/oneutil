f84be175f27c1101938c93aac05ecb42c8ae1c9d
Shawn Lin <xiaoliang.lin@columbia.edu>
2023-07-30 13:25:28 -0400

    add package descriptions

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='oneutil',
-    version='0.0.1',
+    version='0.0.2',
     description='OneSquared Utilities',
+    long_description=long_description,
     author='Shawn Lin',
     packages=find_packages(),
+    python_requires='>=3.6',
     install_requires=[
         'boto3>=1.18.0',
     ],
