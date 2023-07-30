3ce65772d58db5533fe82f9f739236dc3e1358fb
Shawn Lin <xiaoliang.lin@columbia.edu>
2023-07-30 12:15:49 -0400

    add package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oneutil',
+    version='0.0.1',
+    description='OneSquared Utilities',
+    author='Shawn Lin',
+    packages=find_packages(),
+)
